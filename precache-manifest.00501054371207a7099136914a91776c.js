3bbaadefce494a7bf6a8e281588bc0aae4411045
robdacoda <50976565+robdacoda@users.noreply.github.com>
2023-09-14 11:53:48 -0500

    Updates

---
+++ precache-manifest.00501054371207a7099136914a91776c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7063a0b6941f97bc4718",
+    "url": "/fcc_fel2_markdown_previewer/static/css/main.aeb66851.chunk.css"
+  },
+  {
+    "revision": "7063a0b6941f97bc4718",
+    "url": "/fcc_fel2_markdown_previewer/static/js/main.f998e434.chunk.js"
+  },
+  {
+    "revision": "6af412068096bd7afcc7",
+    "url": "/fcc_fel2_markdown_previewer/static/js/runtime~main.ebd8ec3c.js"
+  },
+  {
+    "revision": "f1f37fe57c87b2821a28",
+    "url": "/fcc_fel2_markdown_previewer/static/js/2.39bced16.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/fcc_fel2_markdown_previewer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "72215a2e184fabbe97412e4a65a1b1a0",
+    "url": "/fcc_fel2_markdown_previewer/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fcc_fel2_markdown_previewer/precache-manifest.00501054371207a7099136914a91776c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fcc_fel2_markdown_previewer/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
